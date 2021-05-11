7143110ea664e66e761a7092a8b648d2a89e4015
JEONSUAH <junsu3448@gmail.com>
2021-05-11 09:40:08 +0900

    initial commit

---
+++ app/src/main/java/com/example/lotto/ConstellationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lotto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstellationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constellation_main)
+    }
+}
